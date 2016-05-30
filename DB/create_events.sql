b9a8fa6139a3864db80bbc6cb1abfcd28d14967e
Gabriela Konstanty <gaba.konstanty@gmail.com>
2016-05-30 23:52:37 +0200

    add second database; use docker instead of mysql in host
    Now the only tool required for deploy is docker. But it looks like the
    databases are not always created. So, sometimes some commands from
    deploy.sh needs to be executed for the second time (or whole deploy.sh file)

--- DB/create_events.sql
+++ DB/create_events.sql
@@ -17,18 +17,6 @@ CREATE TABLE Commentary(
 		REFERENCES Event(event_id)
 );
 
-CREATE TABLE User(
-	username varchar(50),
-	password varchar(50),
-	enabled tinyint,
-	role varchar(50),
-	primary key(username)
-);
-
-insert into User(username, password, enabled, role) values('Admin', '123456', 1, 'ROLE_ADMIN');
-insert into User(username, password, enabled, role) values('a', 'a', 1, 'ROLE_ADMIN');
-insert into User(username, password, enabled, role) values('User', '123456', 1, 'ROLE_USER');
-
 insert into Event(title,start_date, content, hashtag) values( 'First event', '2015-01-01', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '#krakow');
 
 insert into Event(title,start_date, content, hashtag) values( 'Second event', '2015-02-01', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '#polska');
--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,6 +2,12 @@
 docker-compose -p siusevents -f ./docker-compose.yml build
 docker-compose -p siusevents -f ./docker-compose.yml up -d
 
-echo "Waiting for the database to boot..."
+echo "Waiting for the databases to boot..."
 sleep 5
-mysql --host=127.0.0.1 --port=3301 --user=sius --password=sius_es events_system < ../DB/create.sql
+# copy file from host to docker; create dababase from file
+docker cp ../DB/create_users.sql siusevents_user_database_1:/create.sql
+docker exec siusevents_user_database_1 bash -c 'mysql --user=sius --password=sius_es events_users < /create.sql'
+
+docker cp ../DB/create_events.sql siusevents_event_database_1:/create.sql
+docker exec siusevents_event_database_1 bash -c 'mysql --user=sius --password=sius_es events_system < /create.sql'
+
