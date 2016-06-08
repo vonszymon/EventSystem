e7207383df9bc76b679d3e6e7d71cedc31498ba2
Andriu <romanski.andrzej@gmail.com>
2016-06-08 13:50:04 +0200

    Expose 8080 for web app

--- docker/run_docker.sh
+++
@@ -1,4 +0,0 @@
-# event_server -> image .cont container
-sudo docker run -d -it --name event_servers.cont event_server bash
-sudo docker start event_servers.cont
-sudo docker run -it event_server bash
