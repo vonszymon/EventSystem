1e8485586aed243d75212e7af51ca5023da2fa96
vonszymon <vonszymon@gmail.com>
2016-06-19 19:05:01 +0200

    Fix DB path

--- docker/Databases/deploy.sh
+++ docker/Databases/deploy.sh
@@ -5,9 +5,9 @@ docker-compose -p siusevents -f ./docker-compose.yml up -d
 echo "Waiting for the databases to boot..."
 sleep 5
 # copy file from host to docker; create dababase from file
-docker cp ../DB/create_users.sql siusevents_user_database_1:/create.sql
+docker cp ../../DB/create_users.sql siusevents_user_database_1:/create.sql
 docker exec siusevents_user_database_1 bash -c 'mysql --user=sius --password=sius_es events_users < /create.sql'
 
-docker cp ../DB/create_events.sql siusevents_event_database_1:/create.sql
+docker cp ../../DB/create_events.sql siusevents_event_database_1:/create.sql
 docker exec siusevents_event_database_1 bash -c 'mysql --user=sius --password=sius_es events_system < /create.sql'
 
