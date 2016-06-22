dcee108943e81ad9599077a25fb49ee9373ba4a2
Szymon Górowski <vonszymon@gmail.com>
2016-06-22 19:10:09 +0200

    Datadog agent

--- docker/start_env.sh
+++ docker/start_env.sh
@@ -1,6 +1,9 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
+docker run -d --name dd-agent -h `hostname` -v /var/run/docker.sock:/var/run/docker.sock -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro \
+-e API_KEY=802efe71870dec9e8c5df14426f3bb81 datadog/docker-dd-agent:latest
+
 cd Databases
 ./deploy.sh
 ./deploy.sh
